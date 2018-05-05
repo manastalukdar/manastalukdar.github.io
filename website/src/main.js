a3edfaf777effa8fa4a1288e8699682ad73626e4
Manas Talukdar <manas.talukdar@gmail.com>
2018-05-05 15:17:31 -0700

    Added vuejs plumbing.

---
+++ website/src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fontawesome from '@fortawesome/fontawesome'
+import brands from '@fortawesome/fontawesome-free-brands'
+
+import { faSpinner } from '@fortawesome/fontawesome-free-solid'
+
+fontawesome.library.add(brands, faSpinner)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
