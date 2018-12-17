fc0c57a5dba5c4971eb9148ba26a01a6433d0780
Manas Talukdar <manas.talukdar@gmail.com>
2018-12-16 20:44:18 -0800

    Updated to new vuejs version and related changes,

--- website/src/main.js
+++ website/src/main.js
@@ -1,18 +1,24 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
-import fontawesome from '@fortawesome/fontawesome'
-import brands from '@fortawesome/fontawesome-free-brands'
 
-import { faSpinner } from '@fortawesome/fontawesome-free-solid'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret, faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon,  FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome'
 
 import VueAnalytics from 'vue-analytics'
 
-fontawesome.library.add(brands, faSpinner)
+library.add(faUserSecret, faSpinner, faLinkedin, faGithub, faTwitter)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.component('font-awesome-layers', FontAwesomeLayers);
+Vue.component('font-awesome-layers-text', FontAwesomeLayersText);
 
 Vue.config.productionTip = false
+
 const isProd = process.env.NODE_ENV === 'production'
 
 Vue.use(VueAnalytics, {
