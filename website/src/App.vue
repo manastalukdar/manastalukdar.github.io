65c77b9bcc0730320a469f17ccf6ecab6090575f
Manas Talukdar <manas.talukdar@gmail.com>
2019-02-18 18:11:00 -0800

    Line in the sand.

--- website/src/App.vue
+++ website/src/App.vue
@@ -8,32 +8,14 @@
       <router-view></router-view>
     </v-content>
 
-    <v-footer
-    dark
-    height="auto"
-  >
-    <v-card
-      class="flex"
-      flat
-      tile
-    >
-      <v-card-actions class="dark darken-2 py-3 justify-center">
-        &copy;&nbsp;{{copyrightStartYear}}—{{copyrightEndYear}}&nbsp;<strong>{{ appOwner }}</strong>
-      </v-card-actions>
-    </v-card>
-  </v-footer>
+    <layout-footer/>
+
   </v-app>
 </template>
 
 <script>
-import { mapState } from 'vuex'
 export default {
   name: 'App',
-  computed: mapState({
-      appOwner: state => state.GlobalData.appOwner,
-      copyrightStartYear: state => state.GlobalData.copyrightStartYear,
-      copyrightEndYear: state => state.GlobalData.copyrightEndYear
-    }),
   components: {
   }
 }
