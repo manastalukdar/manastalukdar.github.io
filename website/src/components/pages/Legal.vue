b6983b6031247e61fe6c200683fa5bc3bf53c7e1
Manas Talukdar <manas.talukdar@gmail.com>
2019-02-22 01:20:13 -0800

    Updated legal text.

--- website/src/components/pages/Legal.vue
+++ website/src/components/pages/Legal.vue
@@ -4,7 +4,7 @@
       wrap
     >
       <v-flex xs12>
-          This a personal website and has nothing to do with my employers, past and present. Any content here or in embedded sites, particularly in the blog section, does not constitute endorsement or guarantees of anything including veracity. I hold the right to determine exclusions. My resume, provided via this site, being an obvious choice.
+          This a personal website and has nothing to do with my employers, past and present. Any content here does not constitute endorsement or guarantees of anything including veracity. I hold the right to determine exclusions, my resume, provided via this site, being an obvious choice.
       </v-flex>
     </v-layout>
   </v-container>
