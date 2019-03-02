0c14079ffca7f30da1561de2f87e35331c29d586
Manas Talukdar <manas.talukdar@gmail.com>
2019-03-01 22:16:17 -0800

    Updated footer.

--- website/src/components/other/footer.vue
+++ website/src/components/other/footer.vue
@@ -5,13 +5,19 @@
     height="auto"
   >
     <v-card
-      class="flex"
+      class="flex dark blue darken-3 py-3"
       flat
       tile
     >
-      <v-card-actions class="dark blue darken-3 py-3 justify-center">
-        &copy;&nbsp; {{ copyrightStartYear }} — {{ copyrightEndYear }} &nbsp;<strong> {{ appOwner }} </strong> &nbsp;||&nbsp; <a href="/sitemap.xml" target="_blank">sitemap</a>
-      </v-card-actions>
+      <v-layout row class="justify-center">
+        <v-flex xs6 text-xs-left style="margin-left: 2em; margin-right: 2em">
+          &copy;&nbsp; {{ copyrightStartYear }} — {{ copyrightEndYear }} &nbsp;<strong> {{ appOwner }} </strong>
+        </v-flex>
+        <v-flex xs6 text-xs-right style="margin-left: 2em; margin-right: 2em">
+          Built with <a href="https://vuejs.org/" target="_blank" style="color:white">Vue.js</a> and <a href="https://nuxtjs.org/" target="_blank" style="color:white">Nuxt.js</a>
+          &nbsp;||&nbsp; <a href="/sitemap.xml" target="_blank" style="color:white">{{ siteMapText }}</a>
+        </v-flex>
+      </v-layout>
     </v-card>
   </v-footer>
 </template>
@@ -19,6 +25,11 @@
 <script>
 import { mapState } from 'vuex'
 export default {
+  data: () => ({
+    siteMapText: 'sitemap',
+    builtWithText:
+      'Built with <a href="https://vuejs.org/" target="_blank">Vue.js</a> and <a href="https://nuxtjs.org/" target="_blank">Nuxt.js</a>'
+  }),
   computed: mapState({
     appOwner: state => state.GlobalData.appOwner,
     copyrightStartYear: state => state.GlobalData.copyrightStartYear,
