da2431c0c29889dcbcbc13695c82a9a6a0acfbf7
Manas Talukdar <manas.talukdar@gmail.com>
2019-03-03 13:15:38 -0800

    Updated legal disclaimer text.

--- website/src/components/pages/legal.vue
+++ website/src/components/pages/legal.vue
@@ -4,7 +4,7 @@
       wrap
     >
       <v-flex xs12>
-        This a personal website and has nothing to do with my employers, past and present. Any content here does not constitute endorsement or guarantees of anything including veracity. I hold the right to determine exclusions, my resume, provided via this site, being an obvious choice.
+        This a personal website and has nothing to do with my employers, past and present. Any content here does not constitute endorsement or guarantees of anything. I hold the right to determine exclusions to this disclaimer. My resume provided via this site being an obvious exclusion given that I stand by all content in it.
       </v-flex>
     </v-layout>
   </v-container>
@@ -19,7 +19,10 @@ export default {
   }),
   head() {
     return {
-      title: this.currentPage + ' || ' + this.appOwner
+      title: this.currentPage + ' || ' + this.appOwner,
+      meta: [
+        { hid: 'description', name: 'description', content: 'Legal disclaimer' }
+      ]
     }
   }
 }
