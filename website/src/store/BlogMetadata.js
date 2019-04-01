ec4ba5bf583db8b348072d77ae4b9cd2a3a8d5e8
Manas Talukdar <manas.talukdar@gmail.com>
2019-03-31 22:53:28 -0700

    Bug fixes.

--- website/src/store/BlogMetadata.js
+++ website/src/store/BlogMetadata.js
@@ -21,7 +21,32 @@ const getters = {
 // actions
 const actions = {
   async getBlogMetadata({ commit }, baseURL) {
-    const { data } = await axios.get(baseURL + '/blogdata/posts_list.json')
+    const { data } = await axios
+      .get(baseURL + '/blogdata/blog_metadata.json')
+      .catch(function(error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          // eslint-disable-next-line no-console
+          console.log(error.response.data)
+          // eslint-disable-next-line no-console
+          console.log(error.response.status)
+          // eslint-disable-next-line no-console
+          console.log(error.response.headers)
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          // eslint-disable-next-line no-console
+          console.log(error.request)
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          // eslint-disable-next-line no-console
+          console.log('Error', error.message)
+        }
+        // eslint-disable-next-line no-console
+        console.log(error.config)
+      })
     commit('setBlogMetadata', data)
   }
 }
