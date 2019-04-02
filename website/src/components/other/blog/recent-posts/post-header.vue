a76c464a8c662d29fc20069ccba45f43adc42c39
Manas Talukdar <manas.talukdar@gmail.com>
2019-04-02 00:47:13 -0700

    Update.

--- website/src/components/other/blog/recent-posts/post-header.vue
+++ website/src/components/other/blog/recent-posts/post-header.vue
@@ -1,12 +1,27 @@
 <template>
   <v-container>
     <v-layout
-      text-xs-center
       wrap
     >
       <v-flex xs12>
-        Under construction.
+        <v-layout row justify-center class="title">
+          <p>
+            {{ postMetadata.title }}
+          </p>
+        </v-layout>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
+
+<script>
+export default {
+  props: {
+    postMetadata: {
+      type: Object,
+      required: true,
+      default: () => {}
+    }
+  }
+}
+</script>
