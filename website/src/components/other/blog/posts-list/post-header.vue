c7785725562c457f39fcf346f08c286c5c2736bf
Manas Talukdar <manas.talukdar@gmail.com>
2019-04-02 00:44:47 -0700

    Refactor.

---
+++ website/src/components/other/blog/posts-list/post-header.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-container>
+    <v-layout
+      wrap
+    >
+      <v-flex xs12>
+        <v-layout row justify-center class="title">
+          <p>
+            {{ postMetadata.title }}
+          </p>
+        </v-layout>
+        <v-layout row justify-center>
+          Authors:&nbsp;
+          <div
+            v-for="item in postMetadata.authors"
+            :key="item['name']"
+          >
+            <nuxt-link
+              :to="{ name: 'blog-author-name', params: { name:item['url-slug'] } }"
+            >
+              <span>{{ item['name'] }}</span>
+            </nuxt-link>
+          &nbsp;
+          </div>
+          &nbsp;
+          || First Published: {{ postMetadata["first-published-on"] }}
+        </v-layout>
+        <v-layout row justify-center>
+          Categories:&nbsp;
+          <div
+            v-for="item in postMetadata.categories"
+            :key="item"
+          >
+            <nuxt-link
+              :to="{ name: 'blog-category-name', params: { name:item } }"
+            >
+              <span>{{ item }}</span>
+            </nuxt-link>
+          &nbsp;
+          </div>
+          &nbsp;|| Tags:&nbsp;
+          <div
+            v-for="item in postMetadata.tags"
+            :key="item"
+          >
+            <nuxt-link
+              :to="{ name: 'blog-tag-name', params: { name:item } }"
+            >
+              <span>{{ item }}</span>
+            </nuxt-link>
+          &nbsp;
+          </div>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: {
+    postMetadata: {
+      type: Object,
+      required: true,
+      default: () => {}
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ website/src/components/other/blog/posts-list/single-post.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <v-layout
+      wrap
+    >
+      <v-flex xs12>
+        <postHeader :post-metadata="postMetadata" />
+        <p />
+        {{ postMetadata.excerpt }}
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import postHeader from './post-header.vue'
+export default {
+  components: {
+    postHeader
+  },
+  props: {
+    postMetadata: {
+      type: Object,
+      required: true,
+      default: () => {}
+    }
+  }
+}
+</script>
