9d31f25585fa0bec4e9a092372999cb5b21e48a7
Manas Talukdar <manas.talukdar@gmail.com>
2019-04-13 00:05:33 -0700

    Added tags and categories pages.

--- website/src/components/pages/blog/categories.vue
+++ website/src/components/pages/blog/categories.vue
@@ -1,9 +1,165 @@
 <template>
   <v-container>
-    <v-layout text-xs-center wrap>
+    <breadcrumbs :breadcrumbs="breadcrumbs" />
+    <p />
+    <v-layout text-xs-justify wrap>
       <v-flex xs12>
-        Under construction.
+        <v-layout row justify-center headline>
+          {{ pageTitle }}
+        </v-layout>
+      </v-flex>
+      <v-flex xs12>
+        <v-card class="ma-3 pa-2" raised elevation="8">
+          <v-card-title>
+            <v-spacer></v-spacer>
+            <v-text-field
+              v-model="search"
+              append-icon="search"
+              label="Search"
+              single-line
+              hide-details
+            ></v-text-field>
+          </v-card-title>
+          <v-data-table :headers="headers" :items="categories" :search="search">
+            <template v-slot:items="props">
+              <td>{{ props.item.category }}</td>
+              <td class="text-xs-right">{{ props.item.count }}</td>
+            </template>
+            <v-alert
+              v-slot:no-results
+              :value="true"
+              color="error"
+              icon="warning"
+            >
+              Your search for "{{ search }}" found no results.
+            </v-alert>
+          </v-data-table>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+import breadcrumbs from '../../other/breadcrumbs'
+export default {
+  components: {
+    breadcrumbs
+  },
+  data() {
+    return {
+      search: '',
+      headers: [
+        {
+          text: 'Category',
+          align: 'left',
+          sortable: true,
+          value: 'category'
+        },
+        { text: 'Number of posts', align: 'right', value: 'count' }
+      ]
+    }
+  },
+  computed: {
+    ...mapState({
+      appOwner: state => state.GlobalData.appOwner,
+      currentPage: state =>
+        state.Navigation.blog.blogText +
+        ' | ' +
+        state.Navigation.blog.blogItems[1].text,
+      blogMetadata: state => state.BlogMetadata.blogMetadata,
+      pageTitle: state => state.Navigation.blog.blogItems[1].text,
+      blogPostsHref: state => state.Navigation.blog.blogItems[0].href,
+      currentHref: state => state.Navigation.blog.blogItems[1].href,
+      categoriesText: state => state.Navigation.blog.blogItems[1].text
+    }),
+    breadcrumbs: function() {
+      return [
+        {
+          text: 'Home',
+          disabled: false,
+          to: '/'
+        },
+        {
+          text: 'Blog Posts',
+          disabled: false,
+          to: this.blogPostsHref
+        },
+        {
+          text: this.categoriesText,
+          disabled: false,
+          to: this.currentHref
+        }
+      ]
+    },
+    cardColor() {
+      return this.$store.getters['GlobalData/getCardColor']
+    }
+  },
+  async asyncData({ store, params, env, payload }) {
+    if (payload) {
+      return {
+        baseUrl: env.baseURL,
+        categories: payload
+      }
+    } else {
+      if (store.state.BlogMetadata.blogMetadata.length === 0) {
+        await store.dispatch('BlogMetadata/getBlogMetadata', [env.baseURL])
+      }
+      const categories = store.getters['BlogMetadata/getCategories']
+      if (categories === undefined) {
+        return {
+          baseUrl: env.baseURL,
+          categories: []
+        }
+      }
+      return {
+        baseUrl: env.baseURL,
+        categories: categories
+      }
+    }
+  },
+  head() {
+    const title = this.currentPage + ' || ' + this.appOwner
+    const description = 'List of all categories from blog.'
+    const url = this.baseUrl + this.currentHref
+    return {
+      title: title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: description
+        },
+        {
+          hid: 'apple-mobile-web-app-title',
+          name: 'apple-mobile-web-app-title',
+          content: title
+        },
+        {
+          hid: 'og-title',
+          name: 'og:title',
+          property: 'og:title',
+          content: title
+        },
+        {
+          hid: 'og-url',
+          name: 'og:url',
+          property: 'og:url',
+          content: url
+        },
+        {
+          hid: 'og-description',
+          name: 'og:description',
+          property: 'og:description',
+          content: description
+        }
+      ],
+      link: [{ rel: 'canonical', href: url }]
+    }
+  }
+}
+</script>
+
+<style></style>
