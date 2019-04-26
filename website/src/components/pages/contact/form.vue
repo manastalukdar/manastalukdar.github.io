47e48f4dfa85a612ba8ff34637d074b69acecb43
Manas Talukdar <manas.talukdar@gmail.com>
2019-04-26 14:31:18 -0700

    fixed typo

--- website/src/components/pages/contact/form.vue
+++ website/src/components/pages/contact/form.vue
@@ -87,7 +87,7 @@ export default {
     ...mapState({
       appOwner: state => state.GlobalData.appOwner,
       currentPage: state =>
-        state.Navigation.contact.contactForm.Text +
+        state.Navigation.contact.contactForm.text +
         ' | ' +
         state.Navigation.contact.contactText,
       currentHref: state => state.Navigation.contact.contactForm.href
