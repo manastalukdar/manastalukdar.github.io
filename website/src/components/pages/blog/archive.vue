05b27ff2ea0da4693e5b547878a25150516355b0
Manas Talukdar <manas.talukdar@gmail.com>
2019-04-26 11:24:49 -0700

    Added archive page.

--- website/src/components/pages/blog/archive.vue
+++ website/src/components/pages/blog/archive.vue
@@ -1,9 +1,213 @@
 <template>
   <v-container>
-    <v-layout text-xs-center wrap>
+    <breadcrumbs :breadcrumbs="breadcrumbs" />
+    <p />
+    <v-layout text-xs-justify wrap>
       <v-flex xs12>
-        Under construction.
+        <v-layout row justify-center headline>
+          {{ pageTitle }}
+        </v-layout>
+      </v-flex>
+      <v-flex xs12>
+        <v-card class="ma-3 pa-2" raised elevation="8">
+          <v-card-title>
+            <v-spacer></v-spacer>
+            <v-text-field
+              v-model="search"
+              append-icon="search"
+              label="Search"
+              single-line
+              hide-details
+            ></v-text-field>
+          </v-card-title>
+          <v-data-table
+            :headers="headers"
+            :items="blogMetadata"
+            :search="search"
+          >
+            <template v-slot:items="props">
+              <td>
+                <nuxt-link
+                  :to="
+                    getLink(
+                      props.item['first-published-on'],
+                      props.item['url-slug']
+                    )
+                  "
+                  >{{ props.item.title }}</nuxt-link
+                >
+              </td>
+            </template>
+            <v-alert
+              v-slot:no-results
+              :value="true"
+              color="error"
+              icon="warning"
+            >
+              Your search for "{{ search }}" found no results.
+            </v-alert>
+          </v-data-table>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
+
+<script>
+import moment from 'moment'
+import { mapState } from 'vuex'
+import breadcrumbs from '../../other/breadcrumbs'
+export default {
+  components: {
+    breadcrumbs
+  },
+  data() {
+    return {
+      search: '',
+      headers: [
+        {
+          text: 'Post Title',
+          align: 'left',
+          sortable: true,
+          value: 'title'
+        }
+      ]
+    }
+  },
+  computed: {
+    ...mapState({
+      appOwner: state => state.GlobalData.appOwner,
+      currentPage: state =>
+        state.Navigation.blog.blogText +
+        ' | ' +
+        state.Navigation.blog.blogItems[4].text,
+      blogMetadata: state => state.BlogMetadata.blogMetadata,
+      pageTitle: state => state.Navigation.blog.blogItems[4].text,
+      blogPostsHref: state => state.Navigation.blog.blogItems[0].href,
+      currentHref: state => state.Navigation.blog.blogItems[4].href,
+      archiveText: state => state.Navigation.blog.blogItems[4].text,
+      blogDynamicItemsBlogPost: state =>
+        state.Navigation.blog.dynamicItems.blogPost.href
+    }),
+    breadcrumbs: function() {
+      return [
+        {
+          text: 'Home',
+          disabled: false,
+          to: '/'
+        },
+        {
+          text: 'Blog Posts',
+          disabled: false,
+          to: this.blogPostsHref
+        },
+        {
+          text: this.archiveText,
+          disabled: false,
+          to: this.currentHref
+        }
+      ]
+    },
+    cardColor() {
+      return this.$store.getters['GlobalData/getCardColor']
+    }
+  },
+  async asyncData({ store, params, env, payload }) {
+    if (payload) {
+      return {
+        baseUrl: env.baseURL,
+        blogMetadata: payload
+      }
+    } else {
+      if (store.state.BlogMetadata.blogMetadata.length === 0) {
+        await store.dispatch('BlogMetadata/getBlogMetadata', [env.baseURL])
+      }
+      return {
+        baseUrl: env.baseURL
+      }
+    }
+  },
+  methods: {
+    getLink(firstPublishedOn, postSlug) {
+      const momentObj = moment(firstPublishedOn)
+      const yearSlug = momentObj.format('YYYY')
+      const monthSlug = momentObj.format('MM')
+      const dateSlug = momentObj.format('DD')
+      return (
+        this.blogDynamicItemsBlogPost +
+        yearSlug +
+        '/' +
+        monthSlug +
+        '/' +
+        dateSlug +
+        '/' +
+        postSlug +
+        '/'
+      )
+    }
+  },
+  head() {
+    const title = this.currentPage + ' || ' + this.appOwner
+    const description = 'List of all blog posts.'
+    const url = this.baseUrl + this.currentHref
+    const breadcrumbsStructuredDataArray = this.breadcrumbs.map(
+      (item, index) => ({
+        '@type': 'ListItem',
+        position: index + 1,
+        item: {
+          '@id': this.baseUrl + item.to,
+          name: item.text
+        }
+      })
+    )
+    const breadcrumbsStructuredData = {
+      '@context': 'http://schema.org',
+      '@type': 'BreadcrumbList',
+      itemListElement: breadcrumbsStructuredDataArray
+    }
+    return {
+      title: title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: description
+        },
+        {
+          hid: 'apple-mobile-web-app-title',
+          name: 'apple-mobile-web-app-title',
+          content: title
+        },
+        {
+          hid: 'og-title',
+          name: 'og:title',
+          property: 'og:title',
+          content: title
+        },
+        {
+          hid: 'og-url',
+          name: 'og:url',
+          property: 'og:url',
+          content: url
+        },
+        {
+          hid: 'og-description',
+          name: 'og:description',
+          property: 'og:description',
+          content: description
+        }
+      ],
+      link: [{ rel: 'canonical', href: url }],
+      __dangerouslyDisableSanitizers: ['script'],
+      script: [
+        {
+          innerHTML: JSON.stringify(breadcrumbsStructuredData),
+          type: 'application/ld+json'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style></style>
