544fe70669b54e43dd3295401248a2d624943492
Manas Talukdar <manas.talukdar@gmail.com>
2019-05-04 13:55:00 -0700

    Added featured component

---
+++ website/src/components/other/home-page/featured.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-layout column wrap>
+    <v-flex xs12>
+      <v-card
+        :color="cardColor"
+        class="pa-3"
+        raised
+        elevation="8"
+        style="height:100%"
+      >
+        <v-layout row justify-center title>
+          <span>Featured</span>
+        </v-layout>
+        <p />
+        <v-layout row justify-center wrap>
+          <nuxt-link to="/blog/tag/engineering-leadership/">
+            Blog Posts on Engineering Leadership
+          </nuxt-link>
+        </v-layout>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  computed: {
+    cardColor() {
+      return this.$store.getters['GlobalData/getCardColor']
+    }
+  }
+}
+</script>
