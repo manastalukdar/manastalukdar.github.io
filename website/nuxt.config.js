05b3693cfe3b522153f57c0b833e645b808345c2
Manas Talukdar <manas.talukdar@gmail.com>
2019-07-19 13:55:53 -0700

    WIP

--- website/nuxt.config.js
+++ website/nuxt.config.js
@@ -316,12 +316,12 @@ export default {
           headerAndFooterColor: colors.blue.darken3,
           cardColor: colors.blueGrey.darken4,
           primary: colors.teal.lighten3, // blue.darken2
-          accent: colors.grey.darken3,
+          /* accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
+          success: colors.green.accent3 */
         },
         light: {
           headerAndFooterColor: colors.blue.lighten4,
