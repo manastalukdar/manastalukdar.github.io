b42d684a785a79ebc296e6efc7443523caface13
Manas Talukdar <manas.talukdar@gmail.com>
2019-07-22 15:35:08 -0700

    Another fix - WIP.

--- website/src/components/other/home-page/social-media-resume.vue
+++ website/src/components/other/home-page/social-media-resume.vue
@@ -1,28 +1,25 @@
 <template>
   <v-col cols="12">
     <v-card color="cardColor" class="pa-3" raised elevation="8">
-      <v-row class="pa-3">
-        <v-list>
-          <v-list-item
-            v-for="item in socialMediaItems"
-            :key="item.text"
-            :href="item.href"
-            :target="item.target"
-          >
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item>
-        </v-list>
+      <v-row class="pa-3" justify="center">
+        <v-list-item
+          v-for="item in socialMediaItems"
+          :key="item.text"
+          :href="item.href"
+          :target="item.target"
+          class="col px-2 justify-center"
+        >
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item>
       </v-row>
       <v-row class="pa-3" justify="center">
-        <v-list>
-          <v-list-item
-            :key="aboutItems[0].text"
-            :to="aboutItems[0].href"
-            class="text-center"
-          >
-            <v-list-item-title v-text="aboutItems[0].text" />
-          </v-list-item>
-        </v-list>
+        <v-list-item
+          :key="aboutItems[0].text"
+          :to="aboutItems[0].href"
+          class="text-center"
+        >
+          <v-list-item-title v-text="aboutItems[0].text" />
+        </v-list-item>
       </v-row>
     </v-card>
   </v-col>
