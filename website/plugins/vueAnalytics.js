56e5deb78fc51ba2548e7accfe4d98832339c551
Manas Talukdar <manas.talukdar@gmail.com>
2019-09-16 12:18:30 -0700

    cleanup.

--- website/plugins/vueAnalytics.js
+++ website/plugins/vueAnalytics.js
@@ -4,7 +4,6 @@ import VueAnalytics from 'vue-analytics'
 Vue.use(VueAnalytics, {
   id: 'UA-118888630-1',
   debug: {
-    // https://github.com/nuxt-community/analytics-module/issues/57
-    sendHitTask: true // process.env.NODE_ENV === 'production'
+    sendHitTask: process.env.NODE_ENV === 'production'
   }
 })
