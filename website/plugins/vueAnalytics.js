9ce3dd8b68f065a77795c5fe6580b51c40476c00
Manas Talukdar <manas.talukdar@gmail.com>
2019-09-18 15:37:42 -0700

    Minor update.

--- website/plugins/vueAnalytics.js
+++ website/plugins/vueAnalytics.js
@@ -4,6 +4,7 @@ import VueAnalytics from 'vue-analytics'
 Vue.use(VueAnalytics, {
   id: 'UA-118888630-1',
   debug: {
+    enabled: false,
     sendHitTask: process.env.NODE_ENV === 'production'
   }
 })
