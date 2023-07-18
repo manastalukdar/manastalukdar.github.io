9a68c3d565a17f6e0010d626d324868d0719317d
Manas Talukdar <manas.talukdar@gmail.com>
2023-07-18 14:15:57 -0700

    Updates.

--- website/pages/about/recruiters.vue
+++ website/pages/about/recruiters.vue
@@ -43,7 +43,8 @@ const recruiters = computedAsync(async () => {
 });
 const { paperize } = usePaperizer('printMe',  {
   styles: [
-    '.recruiters-header {text-align: center; justify-content: center !important;} .row {display: flex; flex-wrap: wrap; flex: 1 1 auto;} .col {flex-basis: 0; flex-grow: 1; max-width: 100%} .col-12 {flex: 0 0 100%} html {font-family: "Maven Pro", sans-serif; line-height: 1.8; font-size: 15px}'
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+    '../../style/print-recruiters.css'
   ]
 });
 const print = () => {
