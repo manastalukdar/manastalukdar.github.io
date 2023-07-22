bdb4c277f300256a5f38cab3d04a4bb773bcb25e
Manas Talukdar <manas.talukdar@gmail.com>
2023-07-22 14:26:36 -0700

    Update

--- website/components/main-nav-menu/TopNavBar.vue
+++ website/components/main-nav-menu/TopNavBar.vue
@@ -70,11 +70,11 @@ function flipSidebarVisibility() {
 const setTheme = () => {
   Object.entries(themeLookup).forEach(([k,v]) => {
     if (v === selectedTheme.value) {
-      console.log(selectedTheme.value)
-      console.log("k: " + k)
-      console.log("v: " +v)
+      //console.log(selectedTheme.value)
+      //console.log("k: " + k)
+      //console.log("v: " +v)
       theme.global.name.value = k
-      console.log(theme.global.name.value)
+      //console.log(theme.global.name.value)
     }
   })
   //theme.global.name.value = selectedTheme.value;
