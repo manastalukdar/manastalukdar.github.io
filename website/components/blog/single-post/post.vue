b90834bb93a461a59889b60ead7c4c301cb565f0
Manas Talukdar <manas.talukdar@gmail.com>
2023-12-29 22:08:23 -0800

    mermaid initiatization.

--- website/components/blog/single-post/post.vue
+++ website/components/blog/single-post/post.vue
@@ -3,10 +3,10 @@
     <v-col class="px-2">
       <v-row id="printMe" class="py-3 px-3">
         <v-card color="cardColor" raised elevation="8" class="py-6 px-6 v-row">
-          <postHeader :post-metadata="postMetadata"/>
+          <postHeader :post-metadata="passedProps.postMetadata"/>
           <p />
           <!-- eslint-disable-next-line vue/no-v-html -->
-          <div class="blogPostContent col px-5 pt-5" v-html="postContent" />
+          <div class="blogPostContent col px-5 pt-5" v-html="passedProps.postContent" />
         </v-card>
       </v-row>
 
@@ -14,10 +14,10 @@
         <client-only>
           <socialSharing
             :url="url"
-            :title="postMetadata.title"
-            :description="postMetadata.description"
-            :quote="postMetadata.excerpt"
-            :hashtags="hashtags"
+            :title="passedProps.postMetadata.title"
+            :description="passedProps.postMetadata.description"
+            :quote="passedProps.postMetadata.excerpt"
+            :hashtags="hashtags()"
           />
         </client-only>
       </v-row>
@@ -29,56 +29,50 @@
   </v-container>
 </template>
 
-<script>
+<script setup>
 import postHeader from './post-header.vue'
 import socialSharing from './social-sharing.vue'
 import comments from './comments.vue'
-export default {
-  components: {
-    postHeader,
-    socialSharing,
-    comments,
+import mermaid from 'mermaid'
+mermaid.initialize({ startOnLoad: false });
+await mermaid.run()
+const passedProps = defineProps({
+  postMetadata: {
+    type: Object,
+    required: true,
+    default: () => {},
   },
-  props: {
-    postMetadata: {
-      type: Object,
-      required: true,
-      default: () => {},
-    },
-    postContent: {
-      type: String,
-      required: true,
-      default: '',
-    },
-    url: {
-      type: String,
-      required: true,
-      default: '',
-    },
-    postId: {
-      type: String,
-      required: true,
-      default: '',
-    },
+  postContent: {
+    type: String,
+    required: true,
+    default: '',
   },
-  computed: {
-    hashtags() {
-      const hashtagsArray = []
-      this.postMetadata.categories.forEach((category) => {
-        const noSpecialCharNoSpaces = category.name
-          .replace('[^a-zA-Z0-9]+', '')
-          .replace(/\s+/g, '')
-        hashtagsArray.push(noSpecialCharNoSpaces)
-      })
-      this.postMetadata.tags.forEach((tag) => {
-        const noSpecialCharNoSpaces = tag.name
-          .replace('[^a-zA-Z0-9]+', '')
-          .replace(/\s+/g, '')
-        hashtagsArray.push(noSpecialCharNoSpaces)
-      })
-      return hashtagsArray.join()
-    },
+  url: {
+    type: String,
+    required: true,
+    default: '',
   },
+  postId: {
+    type: String,
+    required: true,
+    default: '',
+  },
+});
+const hashtags = () => {
+  const hashtagsArray = []
+  passedProps.postMetadata.categories.forEach((category) => {
+    const noSpecialCharNoSpaces = category.name
+      .replace('[^a-zA-Z0-9]+', '')
+      .replace(/\s+/g, '')
+    hashtagsArray.push(noSpecialCharNoSpaces)
+  })
+  passedProps.postMetadata.tags.forEach((tag) => {
+    const noSpecialCharNoSpaces = tag.name
+      .replace('[^a-zA-Z0-9]+', '')
+      .replace(/\s+/g, '')
+    hashtagsArray.push(noSpecialCharNoSpaces)
+  })
+  return hashtagsArray.join()
 }
 </script>
 
