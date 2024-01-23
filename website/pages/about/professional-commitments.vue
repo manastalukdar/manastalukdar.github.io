001cc144018e740c1fba9d6cafb532327f2f8178
Manas Talukdar <manas.talukdar@gmail.com>
2024-01-22 16:04:57 -0800

    Fixed typo.

--- website/pages/about/professional-commitments.vue
+++ website/pages/about/professional-commitments.vue
@@ -13,7 +13,7 @@
         id="printMe"
       >
         <v-row class="text-h5 px-3 py-3 page-header justify-center">
-          <span>Progessional Commitments</span>
+          <span>Professional Commitments</span>
         </v-row>
         <p />
         <!--eslint-disable-next-line vue/no-v-html-->
