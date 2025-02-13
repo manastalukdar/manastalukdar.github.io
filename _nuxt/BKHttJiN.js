f8f140148d254b66f1cb6aa8ba204f683a0917a2
manastalukdar <manastalukdar@users.noreply.github.com>
2025-02-13 18:58:13 +0000

    GitHub CI Updates [ci skip] c0a68927c29b1ad3a33a734ac323d3fa2537a1ab

---
+++ _nuxt/BKHttJiN.js
@@ -0,0 +1,1 @@
+import{b as r}from"./Bfh4h5rC.js";var e=4;function a(o){return r(o,e)}export{a as c};
---
+++ _nuxt/CbEZn76b.js
@@ -0,0 +1,2 @@
+const o=`- Advisory Board member of [AI 2030](https://www.ai2030.org/) [Jan 2024].
+`;export{o as default};
