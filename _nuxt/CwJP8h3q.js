3ea2fa2e7b14de81d77793ad74b1b92caecc6681
manastalukdar <manastalukdar@users.noreply.github.com>
2025-02-19 02:46:18 +0000

    GitHub CI Updates [ci skip] 8219305237d4392532acb6a83890aaf873f353ff

---
+++ _nuxt/CwJP8h3q.js
@@ -0,0 +1,13 @@
+const e=`### Conferences
+
+- Speaking at the [Global Summit on Robotics and Artificial Intelligence](https://robotics.researchconnects.org) (ROBOTICS2025) on \`Techniques and Applications in Large Language Model Evaluation\`. [Valencia, Spain] [April, 2025]
+- Part of the [organizing committee](https://robotics.spectrumconferences.com/committee) and speaking on \`AI Alignment in Frontier Large Language Models\` at the [3rd International Summit on Robotics, Artificial Intelligence and Machine Learning](https://robotics.spectrumconferences.com) (ISRAI 2025). [Dubai, UAE] [September, 2025]
+
+### Podcasts
+
+- Recorded a conversation as an invited guest on a podcast run by a prestigious startup accelerator with almost $4 B in total funding. The conversation was broadly on AI and startups. It is expected to be published in the new year.
+- Recorded a conversation as an invited guest on a leadership podcast run by a well-known leadership coach. The conversation was broadly on leadership and AI. It is expected to be published in the new year.
+
+<!-- ### Workshops 
+### Other -->
+`;export{e as default};
---
+++ _nuxt/DyfBOVdz.js
@@ -0,0 +1,4 @@
+const a=`**Issued by Titan Awards, May 2024**
+
+Manas Talukdar was recognized with a 2024 Titan Business Award in the category of "Executives & Professionals - Innovator of the Year" for [leadership and innovations in AI with patents and adoption](https://thetitanawards.com/winner-info.php?id=3772).
+`;export{a as default};
