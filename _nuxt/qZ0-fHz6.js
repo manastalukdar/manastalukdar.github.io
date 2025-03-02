104a2a0a94b507e13e8b9d0b08f0035218d62e0f
manastalukdar <manastalukdar@users.noreply.github.com>
2025-03-02 01:03:44 +0000

    GitHub CI Updates [ci skip] 4077a439698bceee130d00fbfbea6d91bd8695a1

---
+++ _nuxt/qZ0-fHz6.js
@@ -0,0 +1,3 @@
+const o=`- Fellow of [British Computer Society](https://www.bcs.org) (BCS) [February 2025].
+- Senior Fellow of [AI 2030](https://www.ai2030.org/) [January 2024].
+`;export{o as default};
