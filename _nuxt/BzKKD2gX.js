e1a0d27d2ec2bfedb4010fb436b5d1dd5fb580d1
manastalukdar <manastalukdar@users.noreply.github.com>
2025-03-03 23:54:25 +0000

    GitHub CI Updates [ci skip] e35c309224bea789e6edc01c93a5b435d530f419

---
+++ _nuxt/BzKKD2gX.js
@@ -0,0 +1,4 @@
+const a=`- Successfully completed a 5-year diploma from **[Assam Fine Arts and Crafts Society](https://afacs.in/)**, India.
+- Performance in Science and Mathematics in All India Secondary School Examination in the top 1/8th.
+- Successfully qualified in All-India Mathematics Olympiad conducted by Delhi Association of Mathematics Teachers.
+`;export{a as default};
