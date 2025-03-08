0fd8c928e4d1a275bc4960c8f501e5a1822377b8
manastalukdar <manastalukdar@users.noreply.github.com>
2025-03-08 06:09:20 +0000

    GitHub CI Updates [ci skip] dc2e8be69dc46c7907e663fc91b3164a68a61fa1

---
+++ _nuxt/BPBZl_Jx.js
@@ -0,0 +1,24 @@
+import{p as w}from"./7e_rKhLl.js";import{_ as i,s as B,g as S,o as F,p as z,a as P,b as W,E as x,I as T,e as D,x as E,F as _,G as A,l as v}from"./B5ik26AQ.js";import{p as N}from"./-EoPn13g.js";import"./DE1V-po8.js";import"./gH0Le5Rx.js";import"./C7BAdh2A.js";import"./BYsnOUUO.js";import"./mUWrPCea.js";import"./DlAUqK2U.js";import"./CSxNV6XU.js";import"./CR-ShQFn.js";import"./CXDFu72n.js";import"./dCQ_mSlN.js";import"./BslZxUjH.js";import"./Oih8QHjY.js";var C={packet:[]},h=structuredClone(C),I=A.packet,L=i(()=>{const t=x({...I,..._().packet});return t.showBits&&(t.paddingY+=10),t},"getConfig"),Y=i(()=>h.packet,"getPacket"),G=i(t=>{t.length>0&&h.packet.push(t)},"pushWord"),M=i(()=>{E(),h=structuredClone(C)},"clear"),u={pushWord:G,getPacket:Y,getConfig:L,clear:M,setAccTitle:W,getAccTitle:P,setDiagramTitle:z,getDiagramTitle:F,getAccDescription:S,setAccDescription:B},O=1e4,H=i(t=>{w(t,u);let e=-1,o=[],n=1;const{bitsPerRow:s}=u.getConfig();for(let{start:a,end:r,label:p}of t.blocks){if(r&&r<a)throw new Error(`Packet block ${a} - ${r} is invalid. End must be greater than start.`);if(a!==e+1)throw new Error(`Packet block ${a} - ${r??a} is not contiguous. It should start from ${e+1}.`);for(e=r??a,v.debug(`Packet block ${a} - ${e} with label ${p}`);o.length<=s+1&&u.getPacket().length<O;){const[m,c]=K({start:a,end:r,label:p},n,s);if(o.push(m),m.end+1===n*s&&(u.pushWord(o),o=[],n++),!c)break;({start:a,end:r,label:p}=c)}}u.pushWord(o)},"populate"),K=i((t,e,o)=>{if(t.end===void 0&&(t.end=t.start),t.start>t.end)throw new Error(`Block start ${t.start} is greater than block end ${t.end}.`);return t.end+1<=e*o?[t,void 0]:[{start:t.start,end:e*o-1,label:t.label},{start:e*o,end:t.end,label:t.label}]},"getNextFittingBlock"),R={parse:i(async t=>{const e=await N("packet",t);v.debug(e),H(e)},"parse")},U=i((t,e,o,n)=>{const s=n.db,a=s.getConfig(),{rowHeight:r,paddingY:p,bitWidth:m,bitsPerRow:c}=a,b=s.getPacket(),l=s.getDiagramTitle(),g=r+p,d=g*(b.length+1)-(l?0:r),k=m*c+2,f=T(e);f.attr("viewbox",`0 0 ${k} ${d}`),D(f,d,k,a.useMaxWidth);for(const[$,y]of b.entries())X(f,y,$,a);f.append("text").text(l).attr("x",k/2).attr("y",d-g/2).attr("dominant-baseline","middle").attr("text-anchor","middle").attr("class","packetTitle")},"draw"),X=i((t,e,o,{rowHeight:n,paddingX:s,paddingY:a,bitWidth:r,bitsPerRow:p,showBits:m})=>{const c=t.append("g"),b=o*(n+a)+a;for(const l of e){const g=l.start%p*r+1,d=(l.end-l.start+1)*r-s;if(c.append("rect").attr("x",g).attr("y",b).attr("width",d).attr("height",n).attr("class","packetBlock"),c.append("text").attr("x",g+d/2).attr("y",b+n/2).attr("class","packetLabel").attr("dominant-baseline","middle").attr("text-anchor","middle").text(l.label),!m)continue;const k=l.end===l.start,f=b-2;c.append("text").attr("x",g+(k?d/2:0)).attr("y",f).attr("class","packetByte start").attr("dominant-baseline","auto").attr("text-anchor",k?"middle":"start").text(l.start),k||c.append("text").attr("x",g+d).attr("y",f).attr("class","packetByte end").attr("dominant-baseline","auto").attr("text-anchor","end").text(l.end)}},"drawWord"),j={draw:U},q={byteFontSize:"10px",startByteColor:"black",endByteColor:"black",labelColor:"black",labelFontSize:"12px",titleColor:"black",titleFontSize:"14px",blockStrokeColor:"black",blockStrokeWidth:"1",blockFillColor:"#efefef"},J=i(({packet:t}={})=>{const e=x(q,t);return`
+	.packetByte {
+		font-size: ${e.byteFontSize};
+	}
+	.packetByte.start {
+		fill: ${e.startByteColor};
+	}
+	.packetByte.end {
+		fill: ${e.endByteColor};
+	}
+	.packetLabel {
+		fill: ${e.labelColor};
+		font-size: ${e.labelFontSize};
+	}
+	.packetTitle {
+		fill: ${e.titleColor};
+		font-size: ${e.titleFontSize};
+	}
+	.packetBlock {
+		stroke: ${e.blockStrokeColor};
+		stroke-width: ${e.blockStrokeWidth};
+		fill: ${e.blockFillColor};
+	}
+	`},"styles"),gt={parser:R,db:u,renderer:j,styles:J};export{gt as diagram};
---
+++ _nuxt/DFkBr_l8.js
@@ -0,0 +1,1 @@
+import{U as a,C as n}from"./B5ik26AQ.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
