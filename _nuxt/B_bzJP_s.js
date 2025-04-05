e15921ba580e15f6b7e241b0a0651c3511abaa95
manastalukdar <manastalukdar@users.noreply.github.com>
2025-04-05 02:02:31 +0000

    GitHub CI Updates [ci skip] ac63155c04895e83f24bd2e305ba73dd9e0b01c6

---
+++ _nuxt/B_bzJP_s.js
@@ -0,0 +1,12 @@
+const n=`### Conferences
+
+- Speaking at the [Global Summit on Robotics and Artificial Intelligence](https://robotics.researchconnects.org) (ROBOTICS2025) on \`Techniques and Applications in Large Language Model Evaluation\`. [Valencia, Spain] [April, 2025]
+- Part of the [organizing committee](https://robotics.spectrumconferences.com/committee) and speaking on \`AI Alignment in Frontier Large Language Models\` at the [3rd International Summit on Robotics, Artificial Intelligence and Machine Learning](https://robotics.spectrumconferences.com) (ISRAI 2025). [Frankfurt, Germany] [September, 2025]
+
+### Podcasts
+
+- Recorded a conversation as an invited guest on a podcast run by a senior industry professional in marketing. Discussed impact of AI in industries and on marketing.
+
+<!-- ### Workshops 
+### Other -->
+`;export{n as default};
---
+++ _nuxt/BpZgDWJH.js
@@ -0,0 +1,6 @@
+const s=`
+- Judge for the 2025 [Globee Awards](https://globeeawards.com).
+- [2025 American Business Awards](https://stevieawards.com/aba) (part of the Stevie Awards).
+- [2025 Titan Business Awards](https://thetitanawards.com).
+- [2025 Noble Business Awards](https://nobleawards.com) (part of the Stevie Awards).
+`;export{s as default};
