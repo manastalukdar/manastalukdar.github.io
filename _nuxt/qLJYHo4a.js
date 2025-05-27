75b486cc9a2465f5fdb1d2aa7eadc1cbf78328a5
manastalukdar <manastalukdar@users.noreply.github.com>
2025-05-27 18:14:09 +0000

    GitHub CI Updates [ci skip] 7ed13b4efab2ff008d326df90f840067f84b5382

---
+++ _nuxt/qLJYHo4a.js
@@ -0,0 +1,25 @@
+const e=`## 2025
+
+- [2025 American Business Awards](https://stevieawards.com/aba) (part of the Stevie Awards).
+- [2025 Titan Business Awards](https://thetitanawards.com).
+- [2025 Noble Business Awards](https://nobleawards.com) (part of the Stevie Awards).
+- Served as a judge for the 2025 [Globee Awards](https://globeeawards.com).
+  - [2025 Globee Awards for Artificial Intelligence](https://credential.globeeawards.com/49f56ccc-0641-4fdc-9e2a-9577904692b0#acc.oPpEn2v8)
+  - [2025 Globee Awards for Cybersecurity](https://credential.globeeawards.com/6f832cc5-4cf7-4da1-8895-76058f529486#acc.eJbGBh7q).
+
+## 2024
+
+- Served as a judge for the 2024 [Globee Awards](https://globeeawards.com).
+  - [2024 Globee Awards - Women in Business](https://credential.globeeawards.com/0835ec52-e563-4450-b30f-b8318460bc07#gs.h9u5n7#acc.u6JAqE47)
+  - [2024 Globee Awards - Business](https://credential.globeeawards.com/f824d6ed-ecaa-4167-b77b-28a954843ae9)
+  - [2024 Globee Awards - Leadership](https://credential.globeeawards.com/0337fb7a-80f5-40bc-8699-c835c08e4e53#gs.foptg7)
+  - [2024 Globee Awards - American Business](https://credential.globeeawards.com/5fc13d72-73fd-49b6-a4a3-98c8cfc5f67e#gs.a0pxdb).
+  - [2024 Globee Awards - Technology](https://credential.globeeawards.com/7bdae446-600c-4ebc-a01f-760799e4aa46#gs.9lt4ci).
+  - [2024 Globee Awards - Disruptors](https://credential.globeeawards.com/8c889137-d177-4b30-a276-5c6f5bf10e04#gs.9x241m).
+  - [2024 Globee Awards - Golden Bridge](https://credential.globeeawards.com/a17f3e66-a8a5-461c-b3a9-1839ff0beffb#gs.8ct13k).
+  - [2024 Globee Awards for Customer Excellence](https://credential.globeeawards.com/6d0ec856-011c-44f1-8270-dfd743427802#gs.8clb76).
+  - [2024 Globee Awards for Cybersecurity](https://credential.globeeawards.com/d85bdcc4-c13a-4dcd-9639-d034eb124cc7).
+- [2024 American Business Awards](https://stevieawards.com/ABA) (part of the Stevie Awards).
+- Accepted as a judge for the [Titan Business Awards](https://www.titanawards.com/).
+- Accepted as a Judge for the 2024 [SIIA CODiE Awards](https://siia.net/codie/).
+`;export{e as default};
