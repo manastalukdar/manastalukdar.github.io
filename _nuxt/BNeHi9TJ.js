c0827162a6c1187965016d0121ca229f6e5e0507
manastalukdar <manastalukdar@users.noreply.github.com>
2025-08-02 07:47:55 +0000

    GitHub CI Updates [ci skip] 710d59ee2c07bda3edd0e26c808b9bcd724a3c65

---
+++ _nuxt/BNeHi9TJ.js
@@ -0,0 +1,20 @@
+const e=`### Memberships
+
+- Member of the [Forbes Technology Council](https://councils.forbes.com/profile/Manas-Talukdar-Director-Engineering-Labelbox/3ae01bdd-08d5-497d-a0da-2360cd17d651) [February 2025].
+- Member and Fellow of the [British Computer Society](https://www.bcs.org/) [February 2025].
+- Member of the [Association for Computing Machinery (ACM)](https://www.acm.org) [September 2024].
+- Member of the [Innovative Executives League](https://www.dragonspears.com/innovative-executives-league) [August 2024].
+- Member of the [American Society for AI](https://www.asfai.org) [July 2024].
+- Member of the [Harvard Business Review](https://hbr.org) Advisory Council [July 2024].
+<!--- Member and Ambassador at the [AI Frontier Network](https://aifn.co/) [May 2024 - May 2025].-->
+- Member of the [AI Accelerator Institute](https://www.aiacceleratorinstitute.com) [April 2024].
+- Member of the [MIT Technology Review](https://www.technologyreview.com) Global Insights Panel [Jan 2024 - Feb 2025].
+- Member and Senior Fellow of [AI 2030](https://www.ai2030.org/) [January 2024].
+- Senior member of [IEEE](https://www.ieee.org) [August 2023].
+
+### Other
+
+- Part of [Perplexity Business Fellowship](https://www.perplexity.ai/business-fellowship) [February 2025].
+- [SF Engineering Leadership Community](https://sfelc.com) [August 2024].
+<!--- [Gartner Peer Community](https://www.gartner.com/peer-community/profile/manas-talukdar-UdiPy) [August 2024].-->
+`;export{e as default};
