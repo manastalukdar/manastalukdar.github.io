d1bdc16181db4e58816b7fdee617dd8cddee172a
manastalukdar <manastalukdar@users.noreply.github.com>
2025-08-09 07:16:17 +0000

    GitHub CI Updates [ci skip]
    Topic extraction mode: use-cached
    Reason: no blog changes - use cached metadata
    Triggered by: push 2ffd228e882693a91a159e16c26a9955ae932651

---
+++ _nuxt/B54I6bIw.js
@@ -0,0 +1,1 @@
+import{U as a,C as n}from"./BNsqwc5W.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ _nuxt/CnimQIuA.js
@@ -0,0 +1,4 @@
+const o=`- Senior Fellow focusing on AI at [The Digital Economist](https://www.thedigitaleconomist.com) [April 2025 - March 2026].
+- Fellow of [British Computer Society](https://www.bcs.org) (BCS) [February 2025].
+- Senior Fellow of [AI 2030](https://www.ai2030.org/) [January 2024].
+`;export{o as default};
