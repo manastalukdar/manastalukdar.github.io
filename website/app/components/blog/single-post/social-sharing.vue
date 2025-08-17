b174b8d4002c54e3170cea846b6c3793d361d961
Manas Talukdar <manas.talukdar@gmail.com>
2025-08-17 12:00:55 -0700

    Fixed social sharing errors.

--- website/app/components/blog/single-post/social-sharing.vue
+++ website/app/components/blog/single-post/social-sharing.vue
@@ -3,36 +3,18 @@
     <v-card color="cardColor" raised elevation="8" class="py-4">
       <div class="socialSharing" justify="center">
         <div class="socialSharingItems">
-          <ShareNetwork
-            network="email"
-            :url="url"
-            :title="title"
-            :description="description"
-          >
-            <a><TreeShakenIcon icon="mdi-email" size="24" class="socialSharingItem" /></a>
-          </ShareNetwork>
-          <ShareNetwork network="linkedin" :url="url" :title="title">
-            <a><TreeShakenIcon icon="mdi-linkedin" size="24" class="socialSharingItem" /></a>
-          </ShareNetwork>
-          <ShareNetwork
-            network="twitter"
-            :url="url"
-            :title="title"
-            :hashtags="hashtags"
-            :twitter-user="twitterUser"
-          >
-            <a><TreeShakenIcon icon="mdi-twitter" size="24" class="socialSharingItem" /></a>
-          </ShareNetwork>
-          <ShareNetwork
-            network="facebook"
-            :url="url"
-            :title="title"
-            :description="description"
-            :quote="quote"
-            :hashtags="hashtags"
-          >
-            <a><TreeShakenIcon icon="mdi-facebook" size="24" class="socialSharingItem" /></a>
-          </ShareNetwork>
+          <a :href="emailShare" target="_blank" rel="noopener noreferrer">
+            <TreeShakenIcon icon="mdi-email" size="24" class="socialSharingItem" />
+          </a>
+          <a :href="linkedinShare" target="_blank" rel="noopener noreferrer">
+            <TreeShakenIcon icon="mdi-linkedin" size="24" class="socialSharingItem" />
+          </a>
+          <a :href="twitterShare" target="_blank" rel="noopener noreferrer">
+            <TreeShakenIcon icon="mdi-twitter" size="24" class="socialSharingItem" />
+          </a>
+          <a :href="facebookShare" target="_blank" rel="noopener noreferrer">
+            <TreeShakenIcon icon="mdi-facebook" size="24" class="socialSharingItem" />
+          </a>
         </div>
       </div>
       <div class="printButton col pt-2" justify="center">
@@ -43,9 +25,11 @@
 </template>
 
 <script setup>
+import { computed } from 'vue'
 import { usePaperizer } from 'paperizer'
 import TreeShakenIcon from '~/components/TreeShakenIcon.vue'
-defineProps ({
+
+const props = defineProps({
   url: {
     type: String,
     required: true,
@@ -72,12 +56,43 @@ defineProps ({
     default: '',
   },
 });
+
 const twitterUser = 'ManasTalukdar';
-const { paperize } = usePaperizer('printMe',  {
+
+// Social sharing URLs
+const emailShare = computed(() => {
+  const subject = encodeURIComponent(props.title)
+  const body = encodeURIComponent(`${props.description}\n\n${props.url}`)
+  return `mailto:?subject=${subject}&body=${body}`
+})
+
+const linkedinShare = computed(() => {
+  const url = encodeURIComponent(props.url)
+  const title = encodeURIComponent(props.title)
+  return `https://www.linkedin.com/sharing/share-offsite/?url=${url}&title=${title}`
+})
+
+const twitterShare = computed(() => {
+  const url = encodeURIComponent(props.url)
+  const text = encodeURIComponent(props.title)
+  const hashtags = encodeURIComponent(props.hashtags)
+  const via = encodeURIComponent(twitterUser)
+  return `https://twitter.com/intent/tweet?url=${url}&text=${text}&hashtags=${hashtags}&via=${via}`
+})
+
+const facebookShare = computed(() => {
+  const url = encodeURIComponent(props.url)
+  const quote = encodeURIComponent(props.quote || props.title)
+  const hashtag = props.hashtags ? encodeURIComponent(`#${props.hashtags.split(',')[0]}`) : ''
+  return `https://www.facebook.com/sharer/sharer.php?u=${url}&quote=${quote}&hashtag=${hashtag}`
+})
+
+const { paperize } = usePaperizer('printMe', {
   styles: [
     '/styles/print-blog-post.css'
   ]
 });
+
 const print = () => {
   paperize()
 };
