398cd9d44df2cf6cdaa5c654536cf8f9f3a24d12
Manas Talukdar <manas.talukdar@gmail.com>
2026-03-23 18:15:11 -0700

    Fix blog header spacing for Vuetify 4 gap-based grid.
    Wrap metadata rows (authors, categories, tags) in a single v-col with
    d-flex instead of placing raw divs as direct v-row children. Vuetify 4
    switched from margin-based to gap-based layout (24px gap), causing each
    raw div child to be excessively spaced. Also remove stray <p /> from
    title rows.

--- website/app/components/blog/posts-list/post-header.vue
+++ website/app/components/blog/posts-list/post-header.vue
@@ -30,33 +30,36 @@
             </div>
           </div>
         </v-col>
-        <p />
       </v-row>
       <v-row class="px-3 py-3" justify="center">
-        Authors:&nbsp;
-        <div v-for="item in props.postMetadata.authors" :key="item['name']">
-          <nuxt-link :to="getAuthorRoute(item['url-slug'])">
-            <span>{{ item['name'] }}</span>
-          </nuxt-link>
-          &nbsp;
-        </div>
-        || Published: {{ props.postMetadata['first-published-on'] }} || {{ props.postMetadata["reading-time"]?.text || "1 min read" }}
+        <v-col class="d-flex flex-wrap justify-center align-center pa-0">
+          Authors:&nbsp;
+          <div v-for="item in props.postMetadata.authors" :key="item['name']">
+            <nuxt-link :to="getAuthorRoute(item['url-slug'])">
+              <span>{{ item['name'] }}</span>
+            </nuxt-link>
+            &nbsp;
+          </div>
+          || Published: {{ props.postMetadata['first-published-on'] }} || {{ props.postMetadata["reading-time"]?.text || "1 min read" }}
+        </v-col>
       </v-row>
       <v-row class="px-3" justify="center">
-        Categories:&nbsp;
-        <div v-for="item in props.postMetadata.categories" :key="item['name']">
-          <nuxt-link :to="getCategoryRoute(item['url-slug'])">
-            <span>{{ item['name'] }}</span>
-          </nuxt-link>
-          &nbsp;
-        </div>
-        || Tags:&nbsp;
-        <div v-for="item in props.postMetadata.tags" :key="item['name']">
-          <nuxt-link :to="getTagRoute(item['url-slug'])">
-            <span>{{ item['name'] }}</span>
-          </nuxt-link>
-          &nbsp;
-        </div>
+        <v-col class="d-flex flex-wrap justify-center align-center pa-0">
+          Categories:&nbsp;
+          <div v-for="item in props.postMetadata.categories" :key="item['name']">
+            <nuxt-link :to="getCategoryRoute(item['url-slug'])">
+              <span>{{ item['name'] }}</span>
+            </nuxt-link>
+            &nbsp;
+          </div>
+          || Tags:&nbsp;
+          <div v-for="item in props.postMetadata.tags" :key="item['name']">
+            <nuxt-link :to="getTagRoute(item['url-slug'])">
+              <span>{{ item['name'] }}</span>
+            </nuxt-link>
+            &nbsp;
+          </div>
+        </v-col>
       </v-row>
     </v-col>
   </v-row>
